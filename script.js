e4f0befbce9ed57d9d1ef326eef5db4a186a6021
Edoardo Coda Zabetta <127448679+Edo-coda@users.noreply.github.com>
2023-04-15 11:53:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,252 @@
+/* Javascript image*/
+
+let img = document.querySelector(".toggle_img");
+let link = document.querySelector(".panel_link");
+
+img.addEventListener("mouseover", function () {
+    console.log("over");
+    link.style.textDecoration = "underline #0b5ed7";
+})
+
+img.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img2 = document.querySelector(".toggle_img2");
+let link2 = document.querySelector(".panel_link2");
+
+img2.addEventListener("mouseover", function () {
+    console.log("over");
+    link2.style.textDecoration = "underline #0b5ed7";
+})
+
+img2.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link2.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img3 = document.querySelector(".toggle_img3");
+let link3 = document.querySelector(".panel_link3");
+
+img3.addEventListener("mouseover", function () {
+    console.log("over");
+    link3.style.textDecoration = "underline #0b5ed7";
+})
+
+img3.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link3.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img4 = document.querySelector(".toggle_img4");
+let link4 = document.querySelector(".panel_link4");
+
+img4.addEventListener("mouseover", function () {
+    console.log("over");
+    link4.style.textDecoration = "underline #0b5ed7";
+})
+
+img4.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link4.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img5 = document.querySelector(".toggle_img5");
+let link5 = document.querySelector(".panel_link5");
+
+img5.addEventListener("mouseover", function () {
+    console.log("over");
+    link5.style.textDecoration = "underline #0b5ed7";
+})
+
+img5.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link5.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img6 = document.querySelector(".toggle_img6");
+let link6 = document.querySelector(".panel_link6");
+
+img6.addEventListener("mouseover", function () {
+    console.log("over");
+    link6.style.textDecoration = "underline #0b5ed7";
+})
+
+img6.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link6.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img7 = document.querySelector(".toggle_img7");
+let link7 = document.querySelector(".panel_link7");
+
+img7.addEventListener("mouseover", function () {
+    console.log("over");
+    link7.style.textDecoration = "underline #0b5ed7";
+})
+
+img7.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link7.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img8 = document.querySelector(".toggle_img8");
+let link8 = document.querySelector(".panel_link8");
+
+img8.addEventListener("mouseover", function () {
+    console.log("over");
+    link8.style.textDecoration = "underline #0b5ed7";
+})
+
+img8.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link8.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img9 = document.querySelector(".toggle_img9");
+let link9 = document.querySelector(".panel_link9");
+
+img9.addEventListener("mouseover", function () {
+    console.log("over");
+    link9.style.textDecoration = "underline #0b5ed7";
+})
+
+img9.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link9.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img10 = document.querySelector(".toggle_img10");
+let link10 = document.querySelector(".panel_link10");
+
+img10.addEventListener("mouseover", function () {
+    console.log("over");
+    link10.style.textDecoration = "underline #0b5ed7";
+})
+
+img10.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link10.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img11 = document.querySelector(".toggle_img11");
+let link11 = document.querySelector(".panel_link11");
+
+img11.addEventListener("mouseover", function () {
+    console.log("over");
+    link11.style.textDecoration = "underline #0b5ed7";
+})
+
+img11.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link11.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img12 = document.querySelector(".toggle_img12");
+let link12 = document.querySelector(".panel_link12");
+
+img12.addEventListener("mouseover", function () {
+    console.log("over");
+    link12.style.textDecoration = "underline #0b5ed7";
+})
+
+img12.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link12.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img13 = document.querySelector(".toggle_img13");
+let link13 = document.querySelector(".panel_link13");
+
+img13.addEventListener("mouseover", function () {
+    console.log("over");
+    link13.style.textDecoration = "underline #0b5ed7";
+})
+
+img13.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link13.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img14 = document.querySelector(".toggle_img14");
+let link14 = document.querySelector(".panel_link14");
+
+img14.addEventListener("mouseover", function () {
+    console.log("over");
+    link14.style.textDecoration = "underline #0b5ed7";
+})
+
+img14.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link14.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img15 = document.querySelector(".toggle_img15");
+let link15 = document.querySelector(".panel_link15");
+
+img15.addEventListener("mouseover", function () {
+    console.log("over");
+    link15.style.textDecoration = "underline #0b5ed7";
+})
+
+img15.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link15.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img16 = document.querySelector(".toggle_img16");
+let link16 = document.querySelector(".panel_link16");
+
+img16.addEventListener("mouseover", function () {
+    console.log("over");
+    link16.style.textDecoration = "underline #0b5ed7";
+})
+
+img16.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link16.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img17 = document.querySelector(".toggle_img17");
+let link17 = document.querySelector(".panel_link17");
+
+img17.addEventListener("mouseover", function () {
+    console.log("over");
+    link17.style.textDecoration = "underline #0b5ed7";
+})
+
+img17.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link17.style.textDecoration = "none";
+})
+
+/* ------------ */
+let img18 = document.querySelector(".toggle_img18");
+let link18 = document.querySelector(".panel_link18");
+
+img18.addEventListener("mouseover", function () {
+    console.log("over");
+    link18.style.textDecoration = "underline #0b5ed7";
+})
+
+img18.addEventListener("mouseleave", function () {
+    console.log("leave");
+    link18.style.textDecoration = "none";
+})
